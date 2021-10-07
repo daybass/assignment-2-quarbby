4cae486370639838ebdb766203dcdb16b1d6df03
Adya Danaditya <adya@bagikata.com>
2021-10-07 02:15:20 -0400

    modify sentiment

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 import streamlit as st
 import streamlit.components.v1 as components
 import pandas as pd
+import plotly.express as px
 import numpy as np
 from plotly.graph_objs import Pie, Figure
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
 from PIL import Image
 import matplotlib.pyplot as plt
 from plotly import graph_objs as go
+import altair as alt
 import re
 
-st.set_page_config(page_title=None, page_icon=None, layout='centered', initial_sidebar_state='expanded', menu_items=None)
+st.set_page_config(page_title="Tweet Emotions - Election 2020", page_icon=None, layout='centered', initial_sidebar_state='expanded', menu_items=None)
 
 st.title('Tweet Explorer')
 
@@ -38,7 +40,7 @@ with st.container():
     st.subheader('Start by filtering the tweets by keywords')
     st.markdown("Use this filter to narrow your analysis to tweets containing certain keywords.  \n"
     "*You can try 'Joe Biden', 'Trump', 'Michigan' or 'stopthesteal' to start*")
-    user_input = st.text_input("Keyword Filter")
+    user_input = st.text_input("Keyword Filter", "Joe Biden")
 
 with st.sidebar:
     st.image("https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/flag-united-states_1f1fa-1f1f8.png", width=80)
@@ -91,9 +93,34 @@ if(user_input):
 else:
     st.subheader("Sentiment of tweets in the whole dataset")
 
-total_posneg_df = total_df.groupby([total_df[DATE_COLUMN].dt.date])["posneg"].mean()*100
-total_posneg_df = total_posneg_df.reset_index()
-st.line_chart(total_posneg_df.rename(columns={DATE_COLUMN:'index', 'posneg': 'sentiment'}).set_index('index'))
+conditions = [
+    (total_df['liwc.posemo'] < total_df['liwc.negemo']),
+    (total_df['liwc.posemo'] > total_df['liwc.negemo']),
+    (total_df['liwc.posemo'] == total_df['liwc.negemo'])
+    ]
+
+# create a list of the values we want to assign for each condition
+values = ['negative', 'positive', 'neutral']
+
+# create a new column and use np.select to assign values to it using our lists as arguments
+total_df['sentiment'] = np.select(conditions, values)
+total_df['date'] = total_df[DATE_COLUMN].dt.date
+sentiment_table = total_df.groupby(['date','sentiment']).count()['id'].reset_index()
+c = alt.Chart(sentiment_table).mark_bar(size=20).encode(
+    x='date',
+    y=alt.Y('id', axis=alt.Axis(values=[0,0.5,1]), stack='normalize', title='number of tweets'),
+    color=alt.Color(
+        'sentiment',
+        scale=alt.Scale(
+            domain=['negative','neutral', 'positive'],
+            range=['#de425b', '#ffe9af', '#488f31'])
+        )
+    ).configure_legend(
+      orient='bottom'
+    )
+
+sentiment_table['id'] = sentiment_table['id'].astype('int64', copy=False)
+st.write(c, use_container_width=True)
 
 st.markdown('<p><span style=color:black;font-size:24px;font-weight:bold;>Average Sentiment about </span>' +
             '<span style=color:red;font-size:24px;font-weight:bold;>' + user_input +
@@ -153,7 +180,7 @@ word_cleaning_after = re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)","
 word_cleaning_after = word_cleaning_after.replace(user_input, '')
 
 with row4_1:
-    st.write('Before Elections')
+    st.subheader('Before Elections')
     wordcloud_before = WordCloud(stopwords=stopwords, mask = us_mask, background_color="white", mode="RGB", max_words=100, width=800, height=400).generate(word_cleaning_before)
     fig, ax = plt.subplots(figsize=(5,5))
     image_colors = ImageColorGenerator(us_color)
@@ -162,10 +189,10 @@ with row4_1:
     plt.axis("off")
     st.pyplot(fig)
 
-    st.write(wordcloud_before.words_)
+    st.write(list(wordcloud_before.words_.keys())[:20])
 
 with row4_2:
-    st.write('After Elections')
+    st.subheader('After Elections')
     wordcloud_after = WordCloud(stopwords=stopwords, mask = us_mask, max_font_size=100, background_color="white", mode="RGB", max_words=100).generate(word_cleaning_after)
     image_colors = ImageColorGenerator(us_color)
     plt.tight_layout(pad=0)
@@ -173,7 +200,7 @@ with row4_2:
     plt.axis("off")
     st.pyplot(fig)
 
-    st.write(wordcloud_after.words_)
+    st.write(list(wordcloud_after.words_.keys())[:20])
 
 st.markdown('<p><span style=color:black;font-size:24px;font-weight:bold;>The most popular words surrounding </span>' +
             '<span style=color:red;font-size:24px;font-weight:bold;>' + user_input +
